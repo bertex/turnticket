b1afa7526c4144e3a14cc0a25d753abd8f70708d
albert.renom <albert.renom@fib.upc.edu>
2016-11-17 17:03:02 +0100

    refactor Test

--- src/test/java/tddmicroexercises.turnticketdispenser/TestTicketDispenser.java
+++ src/test/java/tddmicroexercises.turnticketdispenser/TestTicketDispenser.java
@@ -5,8 +5,30 @@ import static org.junit.Assert.assertEquals;
 
 public class TestTicketDispenser {
     @Test
-    public void foo(){
-        assertEquals(0,0);
+    public void a_new_ticket_should_have_the_turn_number_subsequent_to_the_previous_ticket() {
+        // Arrange
+        TicketDispenser ticketDispenser = new TicketDispenser();
+        TurnTicket previousTicket = ticketDispenser.getTurnTicket();
+
+        // Act
+        TurnTicket newTicket = ticketDispenser.getTurnTicket();
+
+        // Assert
+        assertEquals(1, newTicket.getTurnNumber() - previousTicket.getTurnNumber());
+    }
+
+    @Test
+    public void a_new_ticket_should_have_the_turn_number_subsequent_to_the_previous_ticket_from_another_dispenser() {
+        // Arrange
+        TicketDispenser anotherTicketDispenser = new TicketDispenser();
+        TicketDispenser ticketDispenser = new TicketDispenser();
+
+        // Act
+        TurnTicket previousTicket = anotherTicketDispenser.getTurnTicket();
+        TurnTicket newTicket = ticketDispenser.getTurnTicket();
+
+        // Assert
+        assertEquals(1, newTicket.getTurnNumber() - previousTicket.getTurnNumber());
     }
 
 }
