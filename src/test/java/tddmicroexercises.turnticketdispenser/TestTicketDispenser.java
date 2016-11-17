b0e7a0a0babe63a591983e7b81f64a9b2e18d095
albert.renom <albert.renom@fib.upc.edu>
2016-11-17 08:29:16 +0100

    first commit

---
+++ src/test/java/tddmicroexercises.turnticketdispenser/TestTicketDispenser.java
@@ -0,0 +1,12 @@
+package tddmicroexercises.turnticketdispenser;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestTicketDispenser {
+    @Test
+    public void foo(){
+        assertEquals(0,0);
+    }
+
+}
